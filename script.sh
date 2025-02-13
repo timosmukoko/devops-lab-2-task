4e8adfccb427239ae596a2cecfae8e112cb87e46
Timos Mukoko <mukokotimos@yahoo.fr>
2025-02-13 14:03:14 +0000

    script changes

--- script.sh
+++ script.sh
@@ -6,3 +6,21 @@
 #touch fol_1/1_1.txt fol_1/1_2.txt fol_1/1_3.txt
 #touch fol_2/2_1.txt fol_2/2_2.txt fol_2/2_3.txt
 
+#chmod 600 fol_1/1_1.txt fol_1/1_3.txt
+#chmod 600 fol_2/2_1.txt fol_2/2_3.txt
+
+#chmod 777 fol_1/1_2.txt
+#chmod 777 fol_2/2_2.txt
+
+#echo "Week 2 Lab – Bash & Git" > README.md
+
+#echo "Timos Mukoko" >> README.md
+#echo "Student Num: x23329572" >> README.md
+
+#echo "---" >> README.md
+#echo "[devops-lab-2-task](https://github.com/timosmukoko/devops-lab-2-task.git)" >> README.md
+
+#echo "---" >> README.md
+#echo "To run the script.sh run: bash script.sh" >> README.md
+
+echo "Job completed"
